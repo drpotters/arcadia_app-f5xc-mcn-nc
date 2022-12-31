1db3790395ecf5055c2f7c9bfd1c5c44549e48da
Ubuntu <azureuser@appserver-docker-ubuntu-20.kn4a3k13oshuxmvgimbsdkghib.xx.internal.cloudapp.net>
2022-12-31 06:18:12 +0000

    fixes to run workloads in both docker and k8s

--- app2/start.sh
+++ app2/start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/usr/local/sbin/php-fpm -nDOd extension=json.so -d extension=curl.so --fpm-config /usr/local/etc/php-fpm.d/www.conf && /usr/sbin/nginx -g 'daemon off; error_log /dev/stderr info;'
+/usr/sbin/php-fpm7.2 -nDOd extension=json.so -d extension=curl.so --fpm-config /etc/php/7.2/fpm/pool.d/www.conf && /usr/sbin/nginx -g 'daemon off; error_log /dev/stderr info;'
